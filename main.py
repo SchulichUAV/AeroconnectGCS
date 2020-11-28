c9f349710efb809ad8b1239d17021503d036217d
zach-lau <zlau2000@gmail.com>
2020-11-27 20:35:53 -0700

    Clean up imports

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 #! /usr/bin/env python3
 import threading
-import socket
-from pymavlink import mavutil
-from queue import PriorityQueue
 from PlaneController import PlaneController
+from pymavlink import mavutil
 
 def run(plane_address, server_address):
     mavutil.set_dialect("common")
-
-
-    # Resources
     autopilot = mavutil.mavlink_connection(plane_address)
-
     plane_controller = PlaneController(autopilot, server_address)
-
     plane_controller.run()
 
 if __name__ == "__main__":
