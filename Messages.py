8abb61fb091417f8a9f733a2d80cafd1b17f9f41
Zachary Lau <32786812+zach-lau@users.noreply.github.com>
2020-12-13 12:33:37 -0700

    Enable Requests for Location (#3)
    To get regular location updates we send the mav cmd MAV_CMD_SET_MESSAGE_INTERVAL once with the type of message we expect. This requires the introduction of an init function. We also add a new function to read the incoming commmands.

--- Messages.py
+++ Messages.py
@@ -2,11 +2,27 @@
 # Utility classes for commonly used messages
 
 from Jobs import MessageJob
-from MavlinkInterface import mav
+from MavlinkInterface import mav, mavutil
 
 class Heartbeat(MessageJob):
     def __init__(self, connection):
         super().__init__(mav.heartbeat_send, connection, 6, 8, 102, 0, 4, 3)
     def __repr__(self):
         return "Heartbeat"
-        
+
+class StreamRequest(MessageJob):
+    """Request a mesage to be regularly sent from the autopilot."""
+    def __init__(self, connection, message, interval : int):
+        """
+        connection - connection to send request to
+        message - id of the message to request
+        interval - interval between messages in microseconds (us)
+        """
+        super().__init__(
+            mav.command_long_send, connection, 0, 0, 
+            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0, 
+            message, interval, 0, 0, 0, 0, 0
+            )
+        self.message = message
+    def __repr__(self):
+        return f"Request {self.message}"      
